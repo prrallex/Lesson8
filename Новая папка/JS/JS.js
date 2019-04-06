2f9d9db8d28d6687a926eeb406c6a15fcec3c45e
Student <you@example.com>
2019-04-06 13:59:33 +0300

    first commit

---
+++ Новая папка/JS/JS.js
@@ -0,0 +1,36 @@
+// $("#w10").click(function (event) {
+
+// 	var currentWidth = $("#box").width();
+// 	$("#box").width(currentWidth +=10)
+// })
+// $("#w-10").click(function (event) {
+
+// 	var currentWidth = $("#box").width();
+// 	$("#box").width(currentWidth -=10)
+// })
+// $("#h10").click(function (event) {
+
+// 	var currentHeight = $("#box").height();
+// 	$("#box").height(currentHeight +=10)
+// })
+// $("#h-10").click(function (event) {
+
+// 	var currentHeight = $("#box").height();
+// 	$("#box").height(currentHeight -=10)
+// })
+
+
+
+
+
+
+
+// $("#box").click(function(event){
+// 	console.log($)(this).width()
+// 	console.log($(this).innerWidth())
+// 	console.log($(this).innerHeight())
+// 	console.log("------------")               //нихуя не работает
+// 	console.log($(this).outerWidth())
+// 	console.log($(this).outerHeight())
+
+// });
